b43c0829f422ccd8c229084e0b4dcf181d1580a2
berry8838 <bernardinetabattkhc36@gmail.com>
2023-07-20 22:06:06 +0800

    #qaq更新

--- bot/main.py
+++ bot/main.py
@@ -5,13 +5,18 @@
 #
 # uvloop.install()
 from bot import bot
-# 定时任务与基础
-from bot.modules import bot_commands, check_restart, check_ex, leave_delemby, leave_unauth_group, ranks_task, \
-    userplays_rank
-# 面板
-from bot.modules.panel import admin_panel, config_panel, member_panel, server_panel, user_buy
-# 细化
-from bot.modules.commands import admin, close_it, conmands, exchange, kk, myinfo, pro_rev, renew, restart, renewall, \
-    score, start, sync_group
 
-bot.run()
+
+# 定时任务与基础
+
+def main():
+    from bot.modules import bot_commands, check_restart, check_ex, leave_delemby, leave_unauth_group, ranks_task, \
+        userplays_rank
+    # 面板
+    from bot.modules.panel import admin_panel, config_panel, member_panel, server_panel, user_buy
+    # 细化
+    from bot.modules.commands import admin, close_it, conmands, exchange, kk, myinfo, pro_rev, renew, restart, renewall, \
+        score, start, sync_group
+
+
+bot.run(main())

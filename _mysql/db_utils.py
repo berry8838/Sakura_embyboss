6c5554dc765266cfb3796b919ace0c2063d2a604
berry8838 <bernardinetabattkhc36@gmail.com>
2023-07-20 21:43:15 +0800

    删除无用文件

--- _mysql/db_utils.py
+++
@@ -1,8 +0,0 @@
-from config import host, user, pwd, db
-import pymysql
-from dbutils.pooled_db import PooledDB
-
-# 代理池
-POOL = PooledDB(creator=pymysql, maxconnections=20, mincached=5, blocking=True, host=host, port=3306, user=user,
-                password=pwd, database=db,
-                charset='utf8mb4')

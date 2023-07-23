3ac5ae83bed298f0e358d56fc1b6c55136893179
berry8838 <bernardinetabattkhc36@gmail.com>
2023-07-23 22:00:04 +0800

    fix: 退群封禁无法执行

--- bot/modules/leave_delemby.py
+++ bot/modules/leave_delemby.py
@@ -11,15 +11,15 @@ async def leave_del_emby(_, msg):
     tg = msg.from_user.id
     e = sql_get_emby(tg=tg)
     if e is None:
-        return sendMessage(msg, f'✅ [{msg.from_user.first_name}](tg://user?id={tg}) 已经离开了群组')
+        return await sendMessage(msg, f'✅ [{msg.from_user.first_name}](tg://user?id={tg}) 已经离开了群组')
 
     if e.embyid is None:
-        return sendMessage(msg, f'✅ [{msg.from_user.first_name}](tg://user?id={tg}) 已经离开了群组')
+        return await sendMessage(msg, f'✅ [{msg.from_user.first_name}](tg://user?id={tg}) 已经离开了群组')
     if emby.emby_del(id=e.embyid):
         LOGGER.info(f'【退群删号】- {msg.from_user.first_name}-{tg} 已经离开了群组，咕噜噜，ta的账户被吃掉啦！')
-        return sendMessage(msg,
-                           f'✅ [{msg.from_user.first_name}](tg://user?id={tg}) 已经离开了群组，咕噜噜，ta的账户被吃掉啦！')
+        return await sendMessage(msg,
+                                 f'✅ [{msg.from_user.first_name}](tg://user?id={tg}) 已经离开了群组，咕噜噜，ta的账户被吃掉啦！')
     else:
         LOGGER.error(f'【退群删号】- {msg.from_user.first_name}-{tg} 已经离开了群组，但是没能吃掉ta的账户，请管理员检查！')
-        return sendMessage(msg,
-                           f'✅ [{msg.from_user.first_name}](tg://user?id={tg}) 已经离开了群组，但是没能吃掉ta的账户，请管理员检查！')
+        return await sendMessage(msg,
+                                 f'✅ [{msg.from_user.first_name}](tg://user?id={tg}) 已经离开了群组，但是没能吃掉ta的账户，请管理员检查！')

806a7ef9f46540053ca49569bf20ed5642165955
berry8838 <bernardinetabattkhc36@gmail.com>
2023-12-02 22:10:58 +0800

    fix: config.json一起发送备份，发送勿扰

--- bot/modules/backup_db.py
+++ bot/modules/backup_db.py
@@ -1,8 +1,10 @@
 import os
-from bot import bot, prefixes, owner, LOGGER, db_is_docker, db_docker_name, db_host, db_name, db_user, db_pwd, db_backup_dir, db_backup_maxcount
+from bot import bot, prefixes, owner, LOGGER, db_is_docker, db_docker_name, db_host, db_name, db_user, db_pwd, \
+    db_backup_dir, db_backup_maxcount
 from pyrogram import filters
 from bot.func_helper.backup_db_utils import BackupDBUtils
 from bot.func_helper.msg_utils import deleteMessage
+
 
 async def backup_db():
     backup_file = None
@@ -26,6 +28,8 @@ async def backup_db():
             max_backup_count=db_backup_maxcount
         )
     return backup_file
+
+
 async def auto_backup_db():
     LOGGER.info("BOT数据库备份开始")
     backup_file = await backup_db()
@@ -35,12 +39,21 @@ async def auto_backup_db():
             await bot.send_document(
                 chat_id=owner,
                 document=backup_file,
-                caption=f'BOT数据库备份完毕'
+                caption=f'BOT数据库备份完毕',
+                disable_notification=True  # 勿打扰
+            )
+            await bot.send_document(
+                chat_id=owner,
+                document='config.json',
+                caption=f'config备份完毕',
+                disable_notification=True  # 勿打扰
             )
         except Exception as e:
             LOGGER.info(f'发送到owner失败，文件保存在本地')
     else:
         LOGGER.error(f'BOT数据库手动备份失败，请尽快检查相关配置')
+
+
 # bot数据库手动备份
 @bot.on_message(filters.command('backup_db', prefixes) & filters.user(owner))
 async def manual_backup_db(_, msg):

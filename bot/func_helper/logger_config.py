b5b693119d3704fb0eaf5fae1035783297a6f44c
berry8838 <bernardinetabattkhc36@gmail.com>
2024-08-25 19:52:48 +0800

    🔰 No updates

--- bot/func_helper/logger_config.py
+++ bot/func_helper/logger_config.py
@@ -4,27 +4,26 @@ logger_config -
 Author:susu
 Date:2023/12/12
 """
-# logger_config.py
-
 import datetime
+import pytz
 from loguru import logger
 
 # 转换为亚洲上海时区
-# shanghai = pytz.timezone("Asia/Shanghai")
-# Now = datetime.datetime.now(shanghai)
-Now = datetime.datetime.now()
-
-# 使用 add() 方法来配置日志器的输出格式和级别
-"""笔记： rotation：一种条件，指示何时应关闭当前记录的文件并开始新的文件。
-         retention ：过滤旧文件的指令，在循环或程序结束期间会删除旧文件。 方便好用，比loging好"""
-logger.add(f"log/log_{Now:%Y%m%d}.txt", format="{time} - {name} - {level} - {message}", level="INFO", rotation="00:00",
-           retention="30 days")
-
-
-# 为 pyrogram 的日志记录器添加一个新的处理器，只处理 WARNING 级别或更高级别的日志消息
-# logger.add(f"log/log_{Now:%Y%m%d}.txt", format="{time} - {name} - {level} - {message}", level="WARNING",
-#            filter=lambda record: record["name"] == "pyrogram")  没什么用
+shanghai_tz = pytz.timezone("Asia/Shanghai")
+Now = datetime.datetime.now(shanghai_tz)
+log_filename = f"log/log_{Now.strftime('%Y%m%d')}.txt"
+log_format = "{time:YYYY-MM-DD HH:mm:ss.SSS ZZ} | {name} | {level} | {message}"
+# 更新日志配置中的时间格式，确保记录的时间是东八区的时间
+log_config = {
+    "sink": log_filename,
+    "format": log_format,  # 显示时区信息
+    "level": "INFO",
+    "rotation": "00:00",  # rotation：一种条件，指示何时应关闭当前记录的文件并开始新的文件。
+    "retention": "30 days"  # retention ：过滤旧文件的指令，在循环或程序结束期间会删除旧文件。
+}
+logger.add(**log_config)
 
 
 def logu(name):
+    """返回一个绑定名称的日志实例"""
     return logger.bind(name=name)

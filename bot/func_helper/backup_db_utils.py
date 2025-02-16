0f2e664a3f53eba4f34eba655e9099526ac578de
dddddluo <dddddluo@proton.me>
2025-02-16 15:05:15 +0800

    fix: Improve MySQL database backup with skip-ssl and no-tablespaces options

--- bot/func_helper/backup_db_utils.py
+++ bot/func_helper/backup_db_utils.py
@@ -16,23 +16,31 @@ class BackupDBUtils:
             os.makedirs(backup_dir)
         # 根据时间创建当前备份文件
         backup_file = os.path.join(backup_dir, f'{database_name}-{datetime.now().strftime("%Y-%m-%d-%H-%M-%S")}.sql')
-        command = f"mysqldump -h{host} -P{port} -u{user} -p\'{password}\' {database_name} > {backup_file}"
-        process = await asyncio.create_subprocess_shell(command)
+        command = f"mysqldump -h{host} --skip-ssl --no-tablespaces -P{port} -u{user} -p\'{password}\' {database_name} > {backup_file}"
+        skip_ssl_command = f"mysqldump -h{host} --no-tablespaces -P{port} -u{user} -p\'{password}\' {database_name} > {backup_file}"
+        return_code = -1
         try:
+            process = await asyncio.create_subprocess_shell(command)
             await process.communicate()
+            return_code = process.returncode
+            if return_code != 0:
+                LOGGER.warning(f"BOT数据库备份失败，使用 skip-ssl方式尝试备份")
+                process = await asyncio.create_subprocess_shell(skip_ssl_command)
+                await process.communicate()
+                return_code = process.returncode
+            if return_code != 0:
+                LOGGER.error(f"BOT数据库备份失败, error code: {return_code}")
+                return None
+            LOGGER.info(f"BOT数据库备份成功,文件保存为 {backup_file}")
+            # 获取所有备份文件，并且通过时间进行排序
+            all_backups = sorted(glob.glob(os.path.join(backup_dir, f'{database_name}-*.sql')))
+            # 如果超过了当前的备份最大数量，则删除最久的一个
+            while len(all_backups) > max_backup_count:
+                os.remove(all_backups[0])
+                all_backups.pop(0)
         except Exception as e:
             LOGGER.error(f"BOT数据库备份失败, error: {str(e)}")
-            return
-        if process.returncode != 0:
-            LOGGER.error(f"BOT数据库备份失败, error code: {process.returncode}")
-            return
-        LOGGER.info(f"BOT数据库备份成功,文件保存为 {backup_file}")
-        # 获取所有备份文件，并且通过时间进行排序
-        all_backups = sorted(glob.glob(os.path.join(backup_dir, f'{database_name}-*.sql')))
-        # 如果超过了当前的备份最大数量，则删除最久的一个
-        while len(all_backups) > max_backup_count:
-            os.remove(all_backups[0])
-            all_backups.pop(0)
+            return None
         return backup_file
 
     @staticmethod
@@ -45,12 +53,20 @@ class BackupDBUtils:
         backup_file_in_container = f'{database_name}-{datetime.now().strftime("%Y-%m-%d-%H-%M-%S")}.sql'
         backup_file_on_host = os.path.join(backup_dir, backup_file_in_container)
         # 进入容器，使用mysqldump备份文件
-        command = f'docker exec {container_name} sh -c "mysqldump -u{user} -p\'{password}\' {database_name} > {backup_file_in_container}"'
-        process = await asyncio.create_subprocess_shell(command)
+        command = f'docker exec {container_name} sh -c "mysqldump  --no-tablespaces -u{user} -p\'{password}\' {database_name} > {backup_file_in_container}"'
+        skip_ssl_command = f'docker exec {container_name} sh -c "mysqldump --skip-ssl --no-tablespaces -u{user} -p\'{password}\' {database_name} > {backup_file_in_container}"'
+        return_code = -1
         try:
+            process = await asyncio.create_subprocess_shell(command)
             await process.communicate()
-            if process.returncode != 0:
-                LOGGER.error(f"BOT数据库备份失败, error code: {process.returncode}")
+            return_code = process.returncode
+            if return_code != 0:
+                LOGGER.warning(f"BOT数据库备份失败，使用 skip-ssl方式尝试备份")
+                process = await asyncio.create_subprocess_shell(skip_ssl_command)
+                await process.communicate()
+                return_code = process.returncode
+            if return_code != 0:
+                LOGGER.error(f"BOT数据库备份失败, error code: {return_code}")
                 return None
             # 将容器中的备份文件复制到本地
             command = f'docker cp {container_name}:{backup_file_in_container} {backup_file_on_host}'
@@ -58,7 +74,6 @@ class BackupDBUtils:
             await process.communicate()
         except Exception as e:
             LOGGER.error(f"BOT数据库备份失败, error: {str(e)}")
-            return None
         finally:
             # 删除容器中文件
             command = f'docker exec {container_name} rm {backup_file_in_container}'

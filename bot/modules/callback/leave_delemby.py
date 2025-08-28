e077e78ee9be61016eb7e34a37c21a5d5d85bffe
Ronin <68095403+Arrogance-H@users.noreply.github.com>
2025-08-28 23:01:00 +0800

    Update leave_delemby.py

--- bot/modules/callback/leave_delemby.py
+++ bot/modules/callback/leave_delemby.py
@@ -24,12 +24,12 @@ async def leave_del_emby(_, event: ChatMemberUpdated):
                     LOGGER.info(
                         f'【退群删号】- {user_fname}-{user_id} 已经离开了群组，咕噜噜，ta的账户被吃掉啦！')
                     await bot.send_message(chat_id=event.chat.id,
-                                           text=f'✅ [{user_fname}](tg://user?id={user_id}) 已经离开了群组，咕噜噜，ta的账户被吃掉啦！')
+                                           text=f'✅ [{user_fname}](tg://user?id={user_id}) 已与ME断开连接，愿你归来仍是少年！')
                 else:
                     LOGGER.error(
                         f'【退群删号】- {user_fname}-{user_id} 已经离开了群组，但是没能吃掉ta的账户，请管理员检查！')
                     await bot.send_message(chat_id=event.chat.id,
-                                           text=f'❎ [{user_fname}](tg://user?id={user_id}) 已经离开了群组，但是没能吃掉ta的账户，请管理员检查！')
+                                           text=f'❎ [{user_fname}](tg://user?id={user_id}) 已与ME断开连接，但未销户，请管理员检查！')
                 if _open.leave_ban:
                     await bot.ban_chat_member(chat_id=event.chat.id, user_id=user_id)
             except Exception as e:
@@ -52,12 +52,12 @@ async def leave_del_emby(_, event: ChatMemberUpdated):
                     LOGGER.info(
                         f'【退群删号】- {user_fname}-{user_id} 已经离开了群组，咕噜噜，ta的账户被吃掉啦！')
                     await bot.send_message(chat_id=event.chat.id,
-                                           text=f'✅ [{user_fname}](tg://user?id={user_id}) 已经离开了群组，咕噜噜，ta的账户被吃掉啦！')
+                                           text=f'✅ [{user_fname}](tg://user?id={user_id}) 已与ME断开连接，愿你归来仍是少年！')
                 else:
                     LOGGER.error(
                         f'【退群删号】- {user_fname}-{user_id} 已经离开了群组，但是没能吃掉ta的账户，请管理员检查！')
                     await bot.send_message(chat_id=event.chat.id,
-                                           text=f'❎ [{user_fname}](tg://user?id={user_id}) 已经离开了群组，但是没能吃掉ta的账户，请管理员检查！')
+                                           text=f'❎ [{user_fname}](tg://user?id={user_id}) 已与ME断开连接，但未销户，请管理员检查！')
                 if _open.leave_ban:
                     await bot.ban_chat_member(chat_id=event.chat.id, user_id=user_id)
             except Exception as e:

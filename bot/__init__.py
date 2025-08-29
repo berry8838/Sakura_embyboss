9dfe205311c97bcbdb7e6f5ff946348f6198c482
Ronin <68095403+Arrogance-H@users.noreply.github.com>
2025-08-29 23:24:56 +0800

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -66,7 +66,7 @@ lottery = config.lottery
 moviepilot = config.moviepilot
 auto_update = config.auto_update
 api = config.api
-#save_config()
+save_config()
 
 LOGGER.info("配置文件加载完毕")
 from pyrogram.types import BotCommand
